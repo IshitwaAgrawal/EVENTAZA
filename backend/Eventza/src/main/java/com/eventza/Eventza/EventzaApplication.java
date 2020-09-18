9e79065d5ce6d664c972146a500c35c255098e9c
IshitwaAgrawal <ishitwa2000@gmail.com>
2020-09-18 19:25:31 +0530

    project intitialised

---
+++ backend/Eventza/src/main/java/com/eventza/Eventza/EventzaApplication.java
@@ -0,0 +1,13 @@
+package com.eventza.Eventza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventzaApplication.class, args);
+	}
+
+}
---
+++ backend/Eventza/src/test/java/com/eventza/Eventza/EventzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventza.Eventza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
