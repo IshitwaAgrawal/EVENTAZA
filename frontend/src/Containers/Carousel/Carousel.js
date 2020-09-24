dd29b2f16639b3c890e9a3f6a353efbd3c5e47df
Manas-saxena <manassaxena160601@gmail.com>
2020-09-24 22:54:08 +0530

    validation complete and authentication

--- frontend/src/Containers/Carousel/Carousel.js
+++ frontend/src/Containers/Carousel/Carousel.js
@@ -6,19 +6,19 @@ import img from '../../img/img1.jpg'
 class carousel extends Component {
   state = {
     items: [
-      {id: 1, title: 'item #1'},
-      {id: 2, title: 'item #2'},
-      {id: 3, title: 'item #3'},
-      {id: 4, title: 'item #4'},
-      {id: 5, title: 'item #5'}
+      {id: 1, title: 'item #1',src:'https://in.bmscdn.com/showcaseimage/eventimage/chennai-vs-punjab-15-09-2020-06-19-42-227.jpg'},
+      {id: 2, title: 'item #2',src:'https://in.bmscdn.com/showcaseimage/eventimage/chennai-vs-punjab-15-09-2020-06-19-42-227.jpg'},
+      {id: 3, title: 'item #3',src:'https://in.bmscdn.com/showcaseimage/eventimage/chennai-vs-punjab-15-09-2020-06-19-42-227.jpg'},
+      {id: 4, title: 'item #4',src:'https://in.bmscdn.com/showcaseimage/eventimage/chennai-vs-punjab-15-09-2020-06-19-42-227.jpg'},
+      {id: 5, title: 'item #5',src:'https://in.bmscdn.com/showcaseimage/eventimage/chennai-vs-punjab-15-09-2020-06-19-42-227.jpg'}
     ]
   }
 
   render () {
     const { items } = this.state;
     return (
-      <Carousel itemsToShow={2}>
-        {items.map(item => <div className={classes.Carousel} key={item.id}><img src={img}></img></div>)}
+      <Carousel itemsToShow={1} swipeable={false} draggable={false} >
+        {items.map(item => <div className={classes.Carousel} key={item.id}><img height='350px' width='100%' src={item.src}></img></div>)}
       </Carousel>
     )
   }
